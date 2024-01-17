254e8361dda857b80073082074aaa307df6f0c98
patkinghfx <119961670+patkinghfx@users.noreply.github.com>
2024-01-17 16:37:05 -0400

    Completed question 1

---
+++ Lab 1/Module 1/01_04/Survey/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace Survey
+{
+    class Program
+    {
+        static int Main(String[] args)
+    {
+
+        Console.WriteLine("What's your name?");
+            var name = Console.ReadLine();
+
+        Console.WriteLine("How old are you?");
+            var age = Console.ReadLine();
+
+        Console.WriteLine("In what month were you born?");
+            var month = Console.ReadLine();
+
+        Console.WriteLine("Where were you born?");
+            var birthPlace = Console.ReadLine();
+
+        Console.WriteLine("What is your mother's name?");
+            var momName = Console.ReadLine();
+
+        Console.WriteLine($"Your name is {name}");
+        Console.WriteLine($"You are {age} years old.");
+        Console.WriteLine($"You were born in {month}");
+        Console.WriteLine($"You were born in the city of {birthPlace}");
+        Console.WriteLine($"Your mother's name is {momName}");
+        
+        return 0;
+        }
+    }
+}
+
